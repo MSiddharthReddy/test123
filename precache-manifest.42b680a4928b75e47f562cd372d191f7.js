5a6ef6b21206f3f22bb9ad337e0cd24d79a8d47f
MSiddharthReddy <siddu1512@gmail.com>
2019-11-18 17:43:11 -0800

    Updates

---
+++ precache-manifest.42b680a4928b75e47f562cd372d191f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da9a444295e8f70b0aeb2057b255d85",
+    "url": "/test123/index.html"
+  },
+  {
+    "revision": "c280abb4a2ba9bd057d2",
+    "url": "/test123/static/css/main.cb28e80a.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/test123/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "c280abb4a2ba9bd057d2",
+    "url": "/test123/static/js/main.8e34ddc3.chunk.js"
+  },
+  {
+    "revision": "a345dca1d0da4ea0f355",
+    "url": "/test123/static/js/runtime-main.18af94e6.js"
+  }
+]);
